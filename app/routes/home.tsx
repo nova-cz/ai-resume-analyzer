c8ce3d45d3a7001c2be555a853d0362fd7947ccc
Miguel_CR <125812157+nova-cz@users.noreply.github.com>
2025-10-11 20:48:31 -0600

    updating

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
+import { resumes } from '../../constants/index';
+import ResumeCard from "../components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
@@ -15,6 +17,16 @@ export default function Home() {
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
+
+      {resumes.length > 0 ? (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      ) : (
+        <p>No resumes found</p>
+      )}
 
     </section>
   </main>;
