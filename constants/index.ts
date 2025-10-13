47e0553d47847a5cd1215d5de5e0dddcb2ceda57
Miguel_CR <125812157+nova-cz@users.noreply.github.com>
2025-10-13 17:59:43 -0600

    adding auth

--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,7 @@
 
 
 export const resumes: Resume[] = [
-  {
-    id: "1",
+  { id: "1",
     companyName: "Google",
     jobTitle: "Frontend Developer",
     imagePath: "/images/resume_01.png",
@@ -31,8 +30,7 @@ export const resumes: Resume[] = [
       },
     },
   },
-  {
-    id: "2",
+  {  id: "2",
     companyName: "Microsoft",
     jobTitle: "Cloud Engineer",
     imagePath: "/images/resume_02.png",
@@ -63,6 +61,93 @@ export const resumes: Resume[] = [
   },
   {
     id: "3",
+    companyName: "Apple",
+    jobTitle: "iOS Developer",
+    imagePath: "/images/resume_03.png",
+    resumePath: "/resumes/resume_03.pdf",
+    feedback: {
+      overallScore: 75,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  { id: "4",
+    companyName: "Google",
+    jobTitle: "Frontend Developer",
+    imagePath: "/images/resume_01.png",
+    resumePath: "/resumes/resume_01.pdf",
+    feedback: {
+      overallScore: 85,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  {  id: "5",
+    companyName: "Microsoft",
+    jobTitle: "Cloud Engineer",
+    imagePath: "/images/resume_02.png",
+    resumePath: "/resumes/resume_02.pdf",
+    feedback: {
+      overallScore: 55,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  { id: "6",
     companyName: "Apple",
     jobTitle: "iOS Developer",
     imagePath: "/images/resume_03.png",
